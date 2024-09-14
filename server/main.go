7004f6891161df9bef9dcfa25547c5c05a98c2ae
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2024-09-14 15:45:05 -0300

    Criando CSV e fazendo a leitura do csv

--- server/main.go
+++ server/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/danielfmpc/client_go_rpc_server_stream/server/src/pb/department"
 	"google.golang.org/grpc"
@@ -14,6 +18,32 @@ type Server struct {
 }
 
 func (s *Server) ListPerson(req *department.ListPersonRequest, srv department.DepartmentService_ListPersonServer) error {
+	file, err := os.Open("./data.csv")
+	if err != nil {
+		return fmt.Errorf("error on open", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), ";")
+		id, _ := strconv.Atoi(data[0])
+		name := data[1]
+		email := data[2]
+		income, _ := strconv.Atoi(data[3])
+		departmentId, _ := strconv.Atoi(data[4])
+
+		if int32(departmentId) == req.GetDepartmentId() {
+			if err := srv.Send(&department.ListPersonResponse{
+				Id:           int32(id),
+				Name:         name,
+				Email:        email,
+				Income:       int32(income),
+				DepartmentId: int32(departmentId),
+			}); err != nil {
+				return fmt.Errorf("error on send %v", err)
+			}
+		}
+	}
 	return nil
 }
 
